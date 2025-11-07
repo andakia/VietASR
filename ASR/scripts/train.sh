a30b33d7d91ac967f139eea684f2b4b3d6e0eaf5
lodjim <lodjim5@gmail.com>
2025-11-07 21:24:39 +0000

    change the path to bpe model

--- ASR/scripts/train.sh
+++ ASR/scripts/train.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
+bpe_model=${bpe_model:-data/lang_bpe_2000/bpe.model}
+
 python zipformer/train.py \
     --world-size 4 \
     --num-epochs 300 \
@@ -8,8 +10,7 @@ python zipformer/train.py \
     --use-fp16 1 \
     --train-cuts 50h \
     --manifest-dir data/fbank \
-    --bpe-model ${bpe_model} \
-    --bpe-model data/lang_bpe_2000/bpe.model \
+    --bpe-model "${bpe_model}" \
     --max-duration 1000 \
     --enable-musan 0 \
     --exp-dir zipformer/exp \
